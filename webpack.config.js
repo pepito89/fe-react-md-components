4007b974ad735d5e9b1c9bb4a72e3e1830183e8b
Piotr Pladzyk <piotr.pladzyk@gmail.com>
2019-10-11 02:21:51 +0100

    Exclude externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
+const nodeExternals = require('webpack-node-externals');
+
+const path = require('path');
+
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/lib/index.js',
+  externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       }
     ]
   },
@@ -15,9 +20,10 @@ module.exports = {
   },
   */
   output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'index.js'
+    path: path.resolve(__dirname, './dist/lib'),
+    filename: 'index.js',
+    library: '',
+    libraryTarget: 'commonjs'
   },
   /*
   devServer: {
