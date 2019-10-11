f1ebb5be10511a927408d00aa941ede0783619a3
Piotr Pladzyk <piotr.pladzyk@gmail.com>
2019-10-11 01:58:17 +0100

    Change exporting method

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@
 // import App from './App';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-export { default as Button } from './Button';
-export { default as Input } from './Input';
+import Button from './Button';
+import Input from './Input';
+
+export {Button, Input};
+// export { default as Button } from './Button';
+// export { default as Input } from './Input';
