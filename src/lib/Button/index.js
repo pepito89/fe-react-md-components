1a575555fca530f423b12d094f1e27cfe099cca8
Piotr Pladzyk <piotr.pladzyk@gmail.com>
2019-10-12 20:41:44 +0100

    Simplify exports

--- src/lib/Button/index.js
+++ src/lib/Button/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Button from "@material-ui/core/Button"
 
-/*eslint-disable no-unused-expressions*/
 export default props => (
     <Button
       variant = "contained"
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,3 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-import Button from './Button';
-import Input from './Input';
-
-export {Button, Input};
-// export { default as Button } from './Button';
-// export { default as Input } from './Input';
+// export components below
+export { default as Button } from './Button';
+export { default as Input } from './Input';
