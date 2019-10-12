9a2a41339bf6837df4827e4ba517e19ce927fada
Piotr Pladzyk <piotr.pladzyk@gmail.com>
2019-10-12 13:06:51 +0100

    Add webserver functionality

---
+++ dev.webpack.config.js
@@ -0,0 +1,35 @@
+// const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  // externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './build/dist/lib'),
+    publicPath: '/',
+    filename: 'index.js'
+  },
+  /*
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  */
+  devServer: {
+    contentBase: "./build"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    })
+  ]
+};
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Button } from './lib/';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Button>
+          Finally
+        </Button>
+      </div>
+    );
+  }
+};
+
+export default App;
